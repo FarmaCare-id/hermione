25f63deb24a5614b83ec9d6c56a9808feb2716f0
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-06-19 17:12:14 +0700

    update: page routing

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,6 +1,10 @@
 import 'package:farmacare/ui/home/home.dart';
 import 'package:farmacare/ui/login/login.dart';
 import 'package:farmacare/ui/register/register.dart';
+import 'package:farmacare/ui/register/role_select.dart';
+import 'package:farmacare/ui/register/name_input.dart';
+import 'package:farmacare/ui/register/profession_input.dart';
+import 'package:farmacare/ui/register/profession_verif.dart';
 import 'package:farmacare/ui/landing/landing.dart';
 import 'package:farmacare/ui/splash/splash.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +21,10 @@ class Routes {
   static const String landing = '/landing';
   static const String login = '/login';
   static const String register = '/register';
+  static const String roleSelect = '/register/role';
+  static const String nameInput = '/register/name';
+  static const String professionInput = '/register/profession';
+  static const String professionVerif = '/register/profession/verif';
   static const String home = '/home';
   static const String post = '/post';
   static const String reply = '/reply';
@@ -27,6 +35,10 @@ class Routes {
     landing: (BuildContext context) => LandingScreen(),
     login: (BuildContext context) => LoginScreen(),
     register: (BuildContext context) => RegisterScreen(),
+    roleSelect: (BuildContext context) => RoleScreen(),
+    nameInput: (BuildContext context) => NameInputRegisterScreen(),
+    professionInput: (BuildContext context) => ProfessionInputRegisterScreen(),
+    professionVerif: (BuildContext context) => ProfessionVerifRegisterScreen(),
     home: (BuildContext context) => HomeScreen(),
     post: (BuildContext context) => PostScreen(),
     reply: (BuildContext context) => ReplyApp(),
