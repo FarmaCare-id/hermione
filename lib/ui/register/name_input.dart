b4fe6fb58155fe1e0ade32f02ad3dffded972a34
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-06-21 20:14:22 +0700

    feat: ui name input complete

--- lib/ui/register/name_input.dart
+++ lib/ui/register/name_input.dart
@@ -1,14 +1,24 @@
+import 'package:farmacare/constants/colors.dart';
 import 'package:farmacare/stores/form/form_store.dart';
 import 'package:farmacare/stores/theme/theme_store.dart';
+import 'package:farmacare/utils/locale/app_localization.dart';
+import 'package:farmacare/utils/routes/routes.dart';
+import 'package:farmacare/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 class NameInputRegisterScreen extends StatefulWidget {
   @override
-  _NameInputRegisterScreenState createState() => _NameInputRegisterScreenState();
+  _NameInputRegisterScreenState createState() =>
+      _NameInputRegisterScreenState();
 }
 
 class _NameInputRegisterScreenState extends State<NameInputRegisterScreen> {
+  // text controllers:-----------------------------------------------------------
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+
   // stores: ----------------------------------------------------------
   late ThemeStore _themeStore;
   final _store = FormStore();
@@ -19,7 +29,7 @@ class _NameInputRegisterScreenState extends State<NameInputRegisterScreen> {
   }
 
   @override
-  void didChangeDepencencies() {
+  void didChangeDependencies() {
     super.didChangeDependencies();
     _themeStore = Provider.of<ThemeStore>(context);
   }
@@ -27,24 +37,193 @@ class _NameInputRegisterScreenState extends State<NameInputRegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dummy'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(),
       body: _buildBody(),
     );
   }
 
-  _buildBody() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("404 error"))
+  // body methods:--------------------------------------------------------------
+  Widget _buildBody() {
+    return Material(
+      child: Stack(
+        // padding: EdgeInsets.all(16),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  'Tell us about yourself',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(height: 64),
+                Center(),
+                _buildFirstNameField(),
+                SizedBox(height: 8),
+                _buildLastNameField(),
+                SizedBox(height: 256),
+                _buildNextButton(),
+              ],
+            ),
+          ),
+          Observer(
+            builder: (context) {
+              return _store.success
+                  ? navigate(context)
+                  : _showErrorMessage(_store.errorStore.errorMessage);
+            },
+          ),
+          Observer(builder: (context) {
+            return Visibility(
+              visible: _store.loading,
+              child: CustomProgressIndicatorWidget(),
+            );
+          })
         ],
       ),
     );
+  }
+
+  Widget _buildFirstNameField() {
+    return Observer(
+      builder: (context) {
+        return TextField(
+          decoration: InputDecoration(
+            hintText: 'First Name',
+            filled: true,
+            fillColor: Colors.grey[300],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            errorText: _store.formErrorStore.firstName,
+          ),
+          keyboardType: TextInputType.name,
+          textInputAction: TextInputAction.next,
+          controller: _firstNameController,
+          autofocus: false,
+          onChanged: (value) {
+            _store.setUserId(_firstNameController.text);
+          },
+          onSubmitted: (value) {
+            // FocusScope.of(context).requestFocus(_passwordFocusNode);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildLastNameField() {
+    return Observer(
+      builder: (context) {
+        return TextField(
+          decoration: InputDecoration(
+            hintText: 'Last Name',
+            filled: true,
+            fillColor: Colors.grey[300],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            errorText: _store.formErrorStore.lastName,
+          ),
+          keyboardType: TextInputType.name,
+          textInputAction: TextInputAction.next,
+          controller: _lastNameController,
+          autofocus: false,
+          onChanged: (value) {
+            _store.setLastName(_lastNameController.text);
+          },
+          onSubmitted: (value) {
+            // FocusScope.of(context).requestFocus(_passwordFocusNode);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildNextButton() {
+    return Container(
+      padding: EdgeInsets.only(top: 32),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.professionInput);
+        },
+        child: Text(
+          AppLocalizations.of(context).translate('common_next'),
+          style: TextStyle(
+            color: AppColors.white50,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.bluePeriwinkle,
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget navigate(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushNamed(context, Routes.nameInput);
+    });
+    return Container(
+      color: Colors.black54,
+      child: Center(
+        child: Card(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Success!',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // General Methods:-----------------------------------------------------------
+  _showErrorMessage(String message) {
+    if (message.isNotEmpty) {
+      return Container(
+        color: Colors.black54,
+        child: Center(
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                message,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    } else {
+      return SizedBox.shrink();
+    }
   }
 
   @override
