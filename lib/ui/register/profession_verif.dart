e790de1045031fa28a1a20e0c7108be5bd7dc5eb
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-06-22 20:16:44 +0700

    feat: add modal confirmation

--- lib/ui/register/profession_verif.dart
+++ lib/ui/register/profession_verif.dart
@@ -1,6 +1,11 @@
+import 'package:farmacare/constants/colors.dart';
 import 'package:farmacare/stores/form/form_store.dart';
 import 'package:farmacare/stores/theme/theme_store.dart';
+import 'package:farmacare/utils/locale/app_localization.dart';
+import 'package:farmacare/utils/routes/routes.dart';
+import 'package:farmacare/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 class ProfessionVerifRegisterScreen extends StatefulWidget {
@@ -9,6 +14,9 @@ class ProfessionVerifRegisterScreen extends StatefulWidget {
 }
 
 class _ProfessionVerifRegisterScreenState extends State<ProfessionVerifRegisterScreen> {
+  // text controllers:-----------------------------------------------------------
+  TextEditingController _licenseNumberController = TextEditingController();
+  
   // stores: ----------------------------------------------------------
   late ThemeStore _themeStore;
   final _store = FormStore();
@@ -19,7 +27,7 @@ class _ProfessionVerifRegisterScreenState extends State<ProfessionVerifRegisterS
   }
 
   @override
-  void didChangeDepencencies() {
+  void didChangeDependencies() {
     super.didChangeDependencies();
     _themeStore = Provider.of<ThemeStore>(context);
   }
@@ -27,22 +35,265 @@ class _ProfessionVerifRegisterScreenState extends State<ProfessionVerifRegisterS
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dummy'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(),
       body: _buildBody(),
     );
   }
 
-  _buildBody() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("404 error"))
+  // body methods:--------------------------------------------------------------
+  Widget _buildBody() {
+    return Material(
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  'Profession Verification',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.normal,
+                  ),            
+                ),
+                SizedBox(height: 32),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  child: Text(
+                    AppLocalizations.of(context).translate('signup_profession_verif_desc'),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.greyLabel,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),            
+                  ),
+                ),
+                SizedBox(height: 32),
+                _buildLicenceNumberField(),
+                SizedBox(height: 256),
+                _buildNextButton(),
+              ],
+            ),
+          ),
+          Observer(
+            builder: (context) {
+              return Visibility(
+                visible: _store.loading,
+                child: CustomProgressIndicatorWidget(),
+              );
+            },
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildLicenceNumberField() {
+    return Observer(
+      builder: (context) {
+        return TextField(
+          decoration: InputDecoration(
+            labelText: 'License Number',
+            hintText: 'License Number',
+            filled: true,
+            fillColor: Colors.grey[300],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            errorText: _store.formErrorStore.licenseNumber,
+          ),
+          keyboardType: TextInputType.name,
+          textInputAction: TextInputAction.next,
+          controller: _licenseNumberController,
+          autofocus: false,
+          onChanged: (value) {
+            _store.setLicenseNumber(_licenseNumberController.text);
+          },
+          onSubmitted: (value) {
+            FocusScope.of(context).unfocus();
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildNextButton() {
+    return Container(
+      padding: EdgeInsets.only(top: 32),
+      child: ElevatedButton(
+        onPressed: () {
+          // Navigator.pushNamed(context, Routes.professionInput);
+          _showBottomModal(context);
+        },
+        child: Text(
+          AppLocalizations.of(context).translate('common_next'),
+          style: TextStyle(
+            color: AppColors.white50,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.bluePeriwinkle,
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _showBottomModal(context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (builder) {
+        return Container(
+          // height: 300,
+          color: Colors.transparent,
+          child: new Container(
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(10.0),
+                topRight: const Radius.circular(10.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0, // has the effect of softening the shadow
+                  spreadRadius: 0.0, // has the effect of extending the shadow
+                )
+              ],
+            ),
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 5, right: 5),
+                      
+                      child: IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.fromLTRB(35, 25, 35, 25),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: const Color(0xfff8f8f8),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Confirm Your Information',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      RichText(
+                        textAlign: TextAlign.justify,
+                        text: TextSpan(
+                          text: AppLocalizations.of(context).translate('signup_modal_confirm_desc'),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: AppColors.greyLabel,
+                            wordSpacing: 1
+                          )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _buildNextButtonModal(),
+                      _buildCancelButtonModal()
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  Widget _buildNextButtonModal() {
+    return Container(
+      padding: EdgeInsets.only(top: 32),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.professionInput);
+        },
+        child: Text(
+          AppLocalizations.of(context).translate('common_next'),
+          style: TextStyle(
+            color: AppColors.white50,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.bluePeriwinkle,
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCancelButtonModal() {
+    return Container(
+      padding: EdgeInsets.only(top: 16),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text(
+          'Cancel',
+          style: TextStyle(
+            color: AppColors.black800,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.grey[300],
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ),
       ),
     );
   }
