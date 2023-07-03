14660a0b62e308fcc642cdb00dd857deb6a5d954
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-07-03 23:30:05 +0700

    feat: hexcolor

--- lib/main.dart
+++ lib/main.dart
@@ -29,3 +29,15 @@ Future<void> setPreferredOrientations() {
     DeviceOrientation.landscapeLeft,
   ]);
 }
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
